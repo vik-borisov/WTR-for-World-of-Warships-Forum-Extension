50158b3655bfc6b584d45c4e912f0c3d66edeb0f
Viktor Borisov <vik.borisov@outlook.com>
2017-12-05 00:34:34 +0700

    выпуск версии 0.1.1

--- content/ProAlfa.js
+++ content/ProAlfa.js
@@ -26,8 +26,9 @@ var ProAlfa = function(contentMap)
 
         var playerContent = contentMap[stat.name];
         for (var i = 0; i < playerContent.length; i++) {
-        var head = Zepto("<H3 class='ipsComment_author'></H3>");
-
+        var head = Zepto("<H3 class='ipsComment_author'><a></H3>");
+        var link = Zepto("<a href='http://proships.ru/stat/user/" + stat.name +"' target='_blank'></a>");
+        
         var wr = Zepto("<strong>WR: " + stat.wins_percent + "%</strong>   ")[0];
         wr.style.color = colorizeWinrate(stat.wins_percent);
         wr.style.paddingRight = "5px";
@@ -35,8 +36,9 @@ var ProAlfa = function(contentMap)
         var alfa = Zepto("<strong>ALFA: " + Math.round(stat.all_rate) + "</strong>")[0];
         alfa.style.color = colorizeAlfa(stat.all_rate);
 
-        head.append(wr);
-        head.append(alfa);
+        link.append(wr);
+        link.append(alfa);
+        head.append(link);
         head.insertAfter(
             Zepto(playerContent[i])
             .parent()
@@ -102,7 +104,7 @@ var ProAlfa = function(contentMap)
         }
          var playerContent = contentMap[stat.name];
          for(var i = 0; i < playerContent.length; i++){
-            var head = Zepto("<H2 class='cAuthorPane_author'>[" + stat.clan_tag + "]</H2>");
+            var head = Zepto("<H2 class='cAuthorPane_author'><a href='http://proships.ru/stat/clan/" + stat.clan_tag +"' target='_blank'>[" + stat.clan_tag + "]</a></H2>");
             head[0].style.margin = "3px";
     
             head.insertBefore(Zepto(playerContent[i]).parent().parent());
